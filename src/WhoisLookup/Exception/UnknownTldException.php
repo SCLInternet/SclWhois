06af70da9084264b8f8da0c87362c26ddfe32a66
Tom Oram <tom@scl.co.uk>
2013-02-04 10:58:02 +0000

    Switched to MIT license

--- src/WhoisLookup/Exception/UnknownTldException.php
+++ src/WhoisLookup/Exception/UnknownTldException.php
@@ -3,7 +3,7 @@
  * WhoisLookup library (https://github.com/tomphp/WhiosLookup)
  *
  * @link https://github.com/tomphp/BasicSocket for the canonical source repository
- * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3 (GPL-3.0)
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
  */
 namespace WhoisLookup\Exception;
 
